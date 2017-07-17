de50eaf6341d1fc7a61f015b3b01c2a470708cf1
guozengying <572249347@qq.com>
2017-07-17 14:07:38 +0800

    Initial Commit

---
+++ 7-17Animation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  7-17Animation
+//
+//  Created by 今代科技 on 2017/7/17.
+//  Copyright © 2017年 GZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 7-17AnimationTests/__17AnimationTests.m
@@ -0,0 +1,39 @@
+//
+//  __17AnimationTests.m
+//  7-17AnimationTests
+//
+//  Created by 今代科技 on 2017/7/17.
+//  Copyright © 2017年 GZY. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __17AnimationTests : XCTestCase
+
+@end
+
+@implementation __17AnimationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
